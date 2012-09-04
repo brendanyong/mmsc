7bce122e026e3351363ce46f0128d3e3763b2c5a
Brendan Yong <brendanyong@gmail.com>
2012-09-04 23:48:37 +0800

    basic validation added

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,11 @@
 class Project < ActiveRecord::Base
   attr_accessible :body, :excerpt, :location, :published_at, :title
+  validates :title, :presence => true
+  validates :body, :presence =>true
+  validates :excerpt, :presence =>true
+  validates :location, :presence =>true
+
+  def long_title
+  	"#{title} - #{published_at}"
+  end
 end
