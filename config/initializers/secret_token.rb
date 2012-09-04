371961331febf9e2106f5de57fda0683ce774e53
Brendan Yong <brendanyong@gmail.com>
2012-09-04 22:53:58 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mmsc::Application.config.secret_token = 'b90e5594ed93a92dc2b44b1ebfae94d6af7fef6cbcba5a1c12a20862a28856a56c06d590ea914c896ad48b3ace9dd6b18b6d7b4609079912e91ed77e4c728ebd'
